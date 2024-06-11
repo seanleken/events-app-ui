1d2e00166867730d7524bc7f29ad9c3280c631f8
seanleken <seanleken43@gmail.com>
2024-06-11 13:36:49 +0100

    Lint

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,34 +1,31 @@
-'use client';
+"use client";
 
 import { UserProfile, useUser } from "@auth0/nextjs-auth0/client";
-import styles from './Header.module.scss';
+import styles from "./Header.module.scss";
 import { Button } from "@radix-ui/themes";
 
-export default function() {
-    const { user}  = useUser();
+export default function Header() {
+  const { user } = useUser();
 
-    return (
-        <div className={styles.header}>
-            <h4 className={styles.header__title}>Events App</h4>
-            <UserManagement user={user} />
-        </div>
-    )
+  return (
+    <div className={styles.header}>
+      <h4 className={styles.header__title}>Events App</h4>
+      <UserManagement user={user} />
+    </div>
+  );
 }
 
-function UserManagement({user}: {user?: UserProfile}) {
-    return (
-        user 
-            ?
-            <div className={styles.header__authPanel}>
-                <p>Hi {user.name}</p>
-                <a href="/api/auth/logout">
-                    <Button>
-                        Logout
-                    </Button>
-                </a>
-            </div>
-            : <a href="/api/auth/login">
-                <Button>Log In</Button>
-            </a>    
-    )
+function UserManagement({ user }: { user?: UserProfile }) {
+  return user ? (
+    <div className={styles.header__authPanel}>
+      <p>Hi {user.name}</p>
+      <a href="/api/auth/logout">
+        <Button>Logout</Button>
+      </a>
+    </div>
+  ) : (
+    <a href="/api/auth/login">
+      <Button>Log In</Button>
+    </a>
+  );
 }
